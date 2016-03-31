b95377adfb67b6f2e4a6aca0c98e870c924be9ee
Meredith <meredith.lynn.matthews@gmail.com>
2016-03-30 20:39:32 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 $( document ).ready(function() {
-     $(function(){
-      $("#header").load("header.html");
-      $("#footer").load("footer.html"); 
-    });
+		$("#header").load("inc/header.html");
+		$("#footer").load("inc/footer.html");
+		
+});
+
+$(window).bind("load", function() {
+$("#main").load("inc/home.html");
+});
+
+
+$(document).on('click', 'h1', function (e) {
+    $("#main").load("inc/home.html");
+});
+
+$(document).on('click', '#wordpress', function (e) {
+    $("#main").load("inc/wordpress.html");
+});
+
+$(document).on('click', '#javascript', function (e) {
+    $("#main").load("inc/javascript.html");
 });
